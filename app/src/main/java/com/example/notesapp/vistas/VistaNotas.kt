bedc3baac4e795968365d47c92d16358a693aaab
AlexiaAM <alexia77755@gmail.com>
2021-11-24 12:35:27 -0600

    mejoramiento de estilos para notas y tareas

--- app/src/main/java/com/example/notesapp/vistas/VistaNotas.kt
+++ app/src/main/java/com/example/notesapp/vistas/VistaNotas.kt
@@ -1,5 +1,6 @@
 package com.example.notesapp.vistas
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,7 +19,9 @@ import com.example.notesapp.Screen
 import com.example.notesapp.componentes.BarraNavegacion
 import com.example.notesapp.datos.NotasDatabase
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.sp
 import com.example.notesapp.componentes.BarraNavegacionBottom
 
@@ -28,14 +31,14 @@ fun VistaNotas (navController: NavController?, tipo : Int){
     val contex = LocalContext.current;
     val db = NotasDatabase.getDatabase(contex);
     var notas = db.notaDao().getTodos()
-    
+
     if(tipo == 1){
         notas = db.notaDao().getTareas()
     }
     if(tipo == 2){
         notas = db.notaDao().getNotas()
     }
-    
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -46,11 +49,57 @@ fun VistaNotas (navController: NavController?, tipo : Int){
             .fillMaxWidth()
         ){
             items(notas){ nota ->
-                Surface(modifier = Modifier.clickable {
-                    var id = nota.id.toString()
-                    navController?.navigate(Screen.EditarNota.withArgs(id))
-                }) {
-                    Text(text = nota.titulo, fontSize = 24.sp)
+                Surface(modifier =
+                Modifier
+                    .clickable {
+                        var id = nota.id.toString()
+                        navController?.navigate(Screen.EditarNota.withArgs(id))
+                    }
+                    .padding(5.dp)
+                    ,
+                    color = Color.LightGray,
+                    shape = RoundedCornerShape(8.dp),
+
+                    ) {
+                    Column() {
+                        Row() {
+                            if(nota.esTarea == true){
+                                Text(
+                                    text = nota.titulo,
+                                    fontSize = 22.sp,
+                                    modifier = Modifier.padding(
+                                        top = 3.dp,
+                                        start = 5.dp,
+                                        end = 5.dp,
+                                    )
+                                )
+                            }
+                            else {
+                                Text(
+                                    text = nota.titulo,
+                                    fontSize = 22.sp,
+                                    modifier = Modifier.padding(
+                                        top = 5.dp,
+                                        start = 5.dp,
+                                        end = 5.dp,
+                                        bottom = 5.dp
+                                    )
+                                )
+                            }
+                        }
+                        Row() {
+                            if(nota.esTarea == true) {
+                                Text(
+                                    text = nota.fechaLimite,
+                                    modifier = Modifier.padding(
+                                        bottom = 1.dp,
+                                        start = 5.dp,
+                                        end = 5.dp
+                                    )
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
